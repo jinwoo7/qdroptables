24a9370884ac52e41e67f7f28d5bded0bf42b3fa
Jinwoo Yom <jyom@qti.qualcomm.com>
2018-07-07 03:17:38 -0700

    sure why not lol

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
-import boto3
-import pprint
-import logging
+import boto3, pprint, logging, requests, os, sys
 from os.path import isfile
 from os import remove
-from admin import AWS_SERVER_PUBLIC_KEY, AWS_SERVER_SECRET_KEY
 
+# Error Checking
+env_vars = ['QDROPTABLES-KEY', 'AWS_SERVER_PUBLIC_KEY', 'AWS_SERVER_SECRET_KEY']
+def env_check(env_list):
+	for env in env_list:
+		if env not in os.environ:
+			sys.exit('Please set env variable:{}'.format(env))
 
-'''
-1. listen to rabbitmq
-2. check path
-3. upload to S3
-4. analysis
-5. display result
-'''
-session = boto3.Session(aws_access_key_id=AWS_SERVER_PUBLIC_KEY,
-						aws_secret_access_key=AWS_SERVER_SECRET_KEY)
+# Variable Declaration
+face_api_url = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect"
+
+image_url = 'https://how-old.net/Images/faces2/main007.jpg'
+
+headers = {
+    'Content-Type': "application/json",
+    'Ocp-Apim-Subscription-Key': os.environ['QDROPTABLES-KEY']
+}
+
+params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'emotion'
+}
+
+data = {'url': image_url}
+
+session = boto3.Session(aws_access_key_id=os.environ['AWS_SERVER_PUBLIC_KEY'],
+						aws_secret_access_key=os.environ['AWS_SERVER_SECRET_KEY'])
+
 s3 = session.client('s3')
-
-directory = '/some/dir'
 
 def upload_images(path):	
 	bucket_name = 'rating-imgs'
@@ -39,10 +52,23 @@ def upload_images(path):
 	except Exception as e:
 		logging.error('Encountered error uploading {} to bucket {}. Error: {}'.format(path, bucket_name, e))		
 
+def remove_file(path):
+	remove(path)
+
 def check_file(path):
 	# check that file exists & is jpg extension
-	if isfile(path) and path.endswith('.jpg'):
-		upload_images(path)
+	if not isfile(path):
+		system.exit('File {} does not exist'.format(path))
+	elif not path.endswith('.jpg'):
 		remove(path)
-	else:
-		logging.error('File {} does not exist or is not a .jpg file.'.format(path))
+		system.exit('File {} is not a .jpg file.'.format(path))
+
+print('Get Picture')
+
+print('Upload to S3')
+
+print('Delete Local File')
+
+print('Run Analysis')
+
+print('Display Result')
